bfdffa8b68a357530ef31458ff0c9995aa2ef582
Lameck3064 <lame3064@gmail.com>
2026-02-02 23:48:22 +0300

    fix: remove extra closing div tag

--- src/pages/Marketplace.tsx
+++ src/pages/Marketplace.tsx
@@ -2517,7 +2517,6 @@ export default function Marketplace() {
                       )}
                     </div>
                   </div>
-                  </div>
                 </div>
                 
                 {/* Secondary Actions - Floating Bottom on Mobile (optional) */}
